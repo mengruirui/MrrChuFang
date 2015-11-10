8646a8759ceead6cee67d81de862c853371e17cd
mengruirui <1127631535@qq.com>
2015-11-11 00:51:34 +0800

    完成了登录界面

--- MyChuFang/BaseProject/ViewController/XiaChuFang/Login/LoginDetailViewController.m
+++ MyChuFang/BaseProject/ViewController/XiaChuFang/Login/LoginDetailViewController.m
@@ -9,6 +9,11 @@
 #import "LoginDetailViewController.h"
 
 @interface LoginDetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userTextFied;
+@property (weak, nonatomic) IBOutlet UITextField *passWordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *weboBtn;
+@property (weak, nonatomic) IBOutlet UIButton *qqBtn;
+@property (weak, nonatomic) IBOutlet UIButton *doubanBtn;
 
 @end
 
@@ -17,13 +22,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.navigationController.navigationBar.hidden = NO;
+    [self.weboBtn setBackgroundImage:[UIImage imageNamed:@"xinlang"] forState:(UIControlStateNormal)];
+    [self.qqBtn setBackgroundImage:[UIImage imageNamed:@"QQ"] forState:(UIControlStateNormal)];
+    [self.doubanBtn setBackgroundImage:[UIImage imageNamed:@"dou"] forState:(UIControlStateNormal)];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+//登录
+- (IBAction)login:(id)sender {
+    
+}
+//短信登录
+- (IBAction)loginSMS:(id)sender {
+}
+//登录出现问题
+- (IBAction)problem:(id)sender {
+    
+}
+//微博
+- (IBAction)weibo:(id)sender {
+    
+}
+//QQ
 
+- (IBAction)qq:(id)sender {
+    
+}
+//豆瓣
+- (IBAction)douban:(id)sender {
+    
+}
+//用户名
+- (IBAction)userTextField:(UITextField *)sender {
+    [self.passWordTextField becomeFirstResponder];
+}
+//密码
+
+- (IBAction)passWordTextField:(UITextField *)sender {
+    [sender resignFirstResponder];
+}
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
+}
+//返回
+- (IBAction)back:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
 /*
 #pragma mark - Navigation
 
